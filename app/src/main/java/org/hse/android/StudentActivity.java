64e53afed2eed60b9a333a365a3a89a18bd1f3dc
andre <andrey.liskov03@mail.ru>
2024-02-13 01:01:46 +0500

    added teacher activity logic

--- app/src/main/java/org/hse/android/StudentActivity.java
+++ app/src/main/java/org/hse/android/StudentActivity.java
@@ -1,5 +1,6 @@
 package org.hse.android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,31 @@ public class StudentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student);
+    }
+
+    static class Group {
+        private Integer id;
+        private String name;
+
+        public Group(Integer id, String name){
+            this.id = id;
+            this.name = name;
+        }
+        public Integer getId(){
+            return id;
+        }
+        public void setId(Integer id){
+            this.id = id;
+        }
+        @NonNull
+        public String toString(){
+            return name;
+        }
+        public String getName(){
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
--- app/src/main/java/org/hse/android/TeacherActivity.java
+++ app/src/main/java/org/hse/android/TeacherActivity.java
@@ -3,12 +3,83 @@ package org.hse.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 public class TeacherActivity extends AppCompatActivity {
 
+    private Date currentTime;
+    private TextView time;
+    private TextView status;
+    private TextView subject;
+    private TextView cabinet;
+    private TextView corp;
+    private TextView teacher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+
+        final Spinner spinner = findViewById(R.id.spinner);
+        List<StudentActivity.Group> groups = new ArrayList<>();
+        initGroupList(groups);
+
+        ArrayAdapter<?> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, groups);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Object item = adapter.getItem(position);
+                Log.d("TeacherActivity", "selected item: " + item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        time = findViewById(R.id.textTime);
+        initTime();
+
+        status = findViewById(R.id.textStatus);
+        subject = findViewById(R.id.textSubject);
+        cabinet = findViewById(R.id.textCabinet);
+        corp = findViewById(R.id.textCorp);
+        teacher = findViewById(R.id.textTeacher);
+
+        initData();
+    }
+
+    private void initGroupList(List<StudentActivity.Group> groups){
+        groups.add(new StudentActivity.Group(1, "Преподаватель 1"));
+        groups.add(new StudentActivity.Group(2, "Преподаватель 2"));
+    }
+    private void initTime(){
+        currentTime = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        time.setText(simpleDateFormat.format(currentTime));
+    }
+
+    private void initData(){
+        status.setText("Нет пар");
+        subject.setText("Дисциплина");
+        cabinet.setText("Кабинет");
+        corp.setText("Корпус");
+        teacher.setText("Преподаватель");
     }
 }
